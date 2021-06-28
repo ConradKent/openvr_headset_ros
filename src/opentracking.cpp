76499c222e9439a98e2b3eb5852f19fa18a46836
Conrad Kent <conradmichaelkent@gmail.com>
2021-06-28 18:12:04 -0400

    added src/opentracking.cpp, which will act as the new tracking node moving forward.

---
+++ src/opentracking.cpp
@@ -0,0 +1,5 @@
+/* This is empty for now. The old tracking.cpp node was built using
+ * a python code called "cod_edi.py". The new opentracking node will
+ * not do this and will be included in the src folder as a cpp file.
+ * This code will be written from scratch but use a similar structure
+ * to the tracking node built by cod_edi.py */
