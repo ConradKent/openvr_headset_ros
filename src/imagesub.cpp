19310e3d524e0c8698df064a81e8fa4f3e108169
Conrad Kent <conradmichaelkent@gmail.com>
2021-06-29 18:29:24 -0400

    imagesub.cpp updates with notes/code snippets on how the ROS->OpenCV->OpenGL->OpenVR pipeline will work.

--- src/imagesub.cpp
+++ src/imagesub.cpp
@@ -21,6 +21,65 @@ first person view of the turtlebot.
 #include <tf/transform_broadcaster.h> 
 #include <math.h> 
 
+/* Ripped from hellovr_opengl_main.cpp in the openvr distro. Will need to create an opengl texture using an opencv mat with the rbga8 image from one of the eyes of vr_view.
+ * The pipeline will go vr_view->image subscriber->opencv mat->opengl texture->vr::Texture_t->vrcompositor submit for each eye. Probably need to utilize glTexImage2D.
+void CMainApplication::RenderFrame()
+{
+        // for now as fast as possible
+        if ( m_pHMD )
+        {
+                RenderControllerAxes();
+                RenderStereoTargets();
+                RenderCompanionWindow();
+
+                vr::Texture_t leftEyeTexture = {(void*)(uintptr_t)leftEyeDesc.m_nResolveTextureId, vr::TextureType_OpenGL, vr::ColorSpace_Gamma };
+                vr::VRCompositor()->Submit(vr::Eye_Left, &leftEyeTexture );
+                vr::Texture_t rightEyeTexture = {(void*)(uintptr_t)rightEyeDesc.m_nResolveTextureId, vr::TextureType_OpenGL, vr::ColorSpace_Gamma };
+                vr::VRCompositor()->Submit(vr::Eye_Right, &rightEyeTexture );
+        }
+
+        if ( m_bVblank && m_bGlFinishHack )
+        {
+                //$ HACKHACK. From gpuview profiling, it looks like there is a bug where two renders and a present
+                // happen right before and after the vsync causing all kinds of jittering issues. This glFinish()
+                // appears to clear that up. Temporary fix while I try to get nvidia to investigate this problem.
+                // 1/29/2014 mikesart
+                glFinish();
+        }
+
+        // SwapWindow
+        {
+                SDL_GL_SwapWindow( m_pCompanionWindow );
+        }
+
+        // Clear
+        {
+                // We want to make sure the glFinish waits for the entire present to complete, not just the submission
+                // of the command. So, we do a clear here right here so the glFinish will wait fully for the swap.
+                glClearColor( 0, 0, 0, 1 );
+                glClear( GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT );
+        }
+
+        // Flush and wait for swap.
+        if ( m_bVblank )
+        {
+                glFlush();
+                glFinish();
+        }
+
+        // Spew out the controller and pose count whenever they change.
+        if ( m_iTrackedControllerCount != m_iTrackedControllerCount_Last || m_iValidPoseCount != m_iValidPoseCount_Last )
+        {
+                m_iValidPoseCount_Last = m_iValidPoseCount;
+                m_iTrackedControllerCount_Last = m_iTrackedControllerCount;
+
+                dprintf( "PoseCount:%d(%s) Controllers:%d\n", m_iValidPoseCount, m_strPoseClasses.c_str(), m_iTrackedControllerCount );
+        }
+
+        UpdateHMDMatrixPose();
+}
+ */
+
 // define callback function in a class so that data running inside the class can be used globally
 class Listener_image
 {
@@ -29,8 +88,9 @@ public:
 
 	  void callback(const sensor_msgs::ImageConstPtr& msg)
 	    {
-            cv_bridge::toCvShare(msg, "bgr8")->image.copyTo(image); //copy image data to the image under the same class, which will be assign as a pointer
-	    }
+            //copy image data to the image under the same class, which will be assign as a pointer. Use rba8 format as this is what OpenVR requires for rendering.
+            cv_bridge::toCvShare(msg, "rgba8")->image.copyTo(image);
+            }
 
 };
 
@@ -67,10 +127,10 @@ int main(int argc, char **argv)
   image_transport::ImageTransport it(nh);
 
   // define and modify window
-  cv::namedWindow("view",cv::WINDOW_NORMAL); // cv::WINDOW_NORMAL means the window size can be changed
-  cv::moveWindow("view",1921,0);      // move the window to the VIVE monitor, which is the second moniter on the right side of main monitor, and main monitor has a width of 1920
-  cv::setWindowProperty("view",0,1);  // setWindowProperty(window name, type of window property(full screen = 0), value of window property(full screen = 1))
-  cv::startWindowThread();
+  //cv::namedWindow("view",cv::WINDOW_NORMAL); // cv::WINDOW_NORMAL means the window size can be changed
+  //cv::moveWindow("view",1921,0);      // move the window to the VIVE monitor, which is the second moniter on the right side of main monitor, and main monitor has a width of 1920
+  //cv::setWindowProperty("view",0,1);  // setWindowProperty(window name, type of window property(full screen = 0), value of window property(full screen = 1))
+  //cv::startWindowThread();
   
   ros::Rate r(100);
 
@@ -84,32 +144,17 @@ int main(int argc, char **argv)
   ros::Subscriber ctrl_sub = nh.subscribe("control_method", 10, &Listener_ctrlMethod::chatterCallback,&ctrl_methods);
   
 
-  // fial image, left half shows left eye, right half shows right eye
-  cv::Mat image_final(1200,1920, CV_8UC3,cv::Scalar(0,255,255));
+  // final image, left half shows left eye, right half shows right eye
+  cv::Mat image_final_left(1200,1920, CV_8UC3,cv::Scalar(0,255,255));
 
 
   // assign the images under listerner classes as pointers pointing at left or right half of final image
   // so that the copyTo() will copy the data directly to the final image
-  listener_left.image = image_final(cv::Range(0,1200),cv::Range(0,960));
-  listener_right.image = image_final(cv::Range(0,1200),cv::Range(960,1920));
+  //listener_left.image = image_final(cv::Range(0,1200),cv::Range(0,960));
+  //listener_right.image = image_final(cv::Range(0,1200),cv::Range(960,1920));
 
   Vive_Listener vive_data;
   ros::Subscriber sub_vive = nh.subscribe("vrui/vive", 1, &Vive_Listener::callback, &vive_data);
-
-    cv::Point left,left_lower;
-    left.x = 250;
-    left.y = 500;
-    cv::Point right,right_lower;
-    right.x = left.x + 960 - 60;
-    right.y = left.y;
-
-    left_lower = left;
-    right_lower = right;
-   
-    double textsize = 0.8;
-    int thickness=1.7;
-    
-    //cv::VideoWriter video("/home/zhenyushi/catkin_ws/src/vrui_mdf/out.avi",CV_FOURCC('M','J','P','G'),30, cv::Size(1920,1200),true);
 
 
   while(ros::ok())
@@ -117,60 +162,20 @@ int main(int argc, char **argv)
   ros::spinOnce(); 
   // ros::spin() works too, but extra code can run outside the callback function between each spinning if spinOnce() is used
 
-    cv::putText( image_final, ctrl_methods.control_method, left, 0,textsize, cv::Scalar(0,0,255), thickness, 8);
-    cv::putText( image_final, ctrl_methods.control_method, right, 0,textsize, cv::Scalar(0,0,255), thickness, 8);
-
-    left_lower.y = left.y + 60;
-    right_lower.y = right.y + 60;
-
-    char text[255];
-    sprintf(text, "%d . %d", (int)ros::Time::now().sec,(int)ros::Time::now().nsec);
-
-    cv::putText( image_final, text, left_lower, 0,textsize, cv::Scalar(50,0,255), thickness, 8);
-    cv::putText( image_final, text, right_lower, 0,textsize, cv::Scalar(50,0,255), thickness, 8);
-
-/*
-    double roll, pitch, yaw;
-    tf::Quaternion Qua(vive_data.vive.headset.orientation.x,vive_data.vive.headset.orientation.y,vive_data.vive.headset.orientation.z,vive_data.vive.headset.orientation.w);
-    tf::Matrix3x3 m(Qua); //rotation matrix from Quaternion
-    m.getRPY(roll, pitch, yaw); //eular angle form rotation matrix
-
-    left_lower.y = left_lower.y + 60;
-    right_lower.y = right_lower.y + 60;
-
-
-    double yaw_de = (yaw/3.1415926535897)*180;
-
-    std::cout<<yaw_de<<std::endl;
-
-    double fractpart, intpart;
-    fractpart = modf (yaw_de, &intpart);
-
-    fractpart = fractpart*100;
-    char text1[255];
-    sprintf(text1, "Yaw : %d . %d",(int)intpart, (int)fractpart);
-
-    cv::putText( image_final, text1, left_lower, 0,textsize, cv::Scalar(100,0,255), thickness, 8);
-    cv::putText( image_final, text1, right_lower, 0,textsize, cv::Scalar(100,0,255), thickness, 8);
-
-    cv::Point p_up(960+480,0),  p_down(960+480, 1200);
-    cv::line(image_final, p_up, p_down, cv::Scalar(0,255,0), 2, 8, 0);
-*/
     if(listener_left.image.cols!=0 && listener_right.image.cols!=0) 
     {
-
+    //probably need to use waitgetposes here
+    Renderframe()
     //video.write(image_final);
 
-    cv::imshow("view", image_final);
-    cv::waitKey(1); // necessary for imshow()
+    //cv::imshow("view", image_final);
+    //cv::waitKey(1); // necessary for imshow()
+
+
+
     }
   r.sleep();
   }
-
-
-
-
-
 
   return 0;
 }
