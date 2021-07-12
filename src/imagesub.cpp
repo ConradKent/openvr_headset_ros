0043aee00eaafcf26ceb9b1c7c7d21795ceb470f
Conrad Kent <conradmichaelkent@gmail.com>
2021-07-12 16:38:08 -0400

    Updates to imagesub.cpp. Need to fix CMake undefined reference errors.

--- src/imagesub.cpp
+++ src/imagesub.cpp
@@ -1,4 +1,4 @@
-/*
+﻿/*
 
 This new version of imagesub subscribes the images
 from ROS to OpenCV, then it takes the BGR OpenCV
@@ -7,7 +7,7 @@ RGBA8, then it submits those texture2d's to each
 eye of a VR headset using OpenVR.
 
 */
-#include <stdlib.h>
+#include <iostream>
 #include <ros/ros.h>
 #include <image_transport/image_transport.h>
 #include "std_msgs/String.h"
@@ -35,22 +35,77 @@ public:
 
 };
 
-//from https://github.com/matinas/openvrsimplexamples/blob/master/openvrsimplexamples/src/main.cpp
-vr::IVRSystem* vr_system;
-vr::TrackedDevicePose_t tracked_device_pose[vr::k_unMaxTrackedDeviceCount];
-
 /* Don't need Vive msgs for now. May remove later if we stop using a separate node for tracking.
 class Vive_Listener
 {
 public:
-	  vrui_mdf::Vive vive;
+          vrui_mdf::Vive vive;
 
-	  void callback(const vrui_mdf::Vive& msg)
-	    {
-		vive = msg;
-	    }
+          void callback(const vrui_mdf::Vive& msg)
+            {
+                vive = msg;
+            }
 };
 */
+
+//from https://github.com/matinas/openvrsimplexamples/blob/master/openvrsimplexamples/src/main.cpp
+vr::IVRSystem* vr_system;
+vr::TrackedDevicePose_t tracked_device_pose[vr::k_unMaxTrackedDeviceCount];
+
+std::string driver_name, driver_serial;
+std::string tracked_device_type[vr::k_unMaxTrackedDeviceCount];
+int init_OpenVR();
+
+//-----------------------------------------------------------------------------
+// Purpose: helper to get a string from a tracked device property and turn it
+//			into a std::string. FROM https://github.com/matinas/openvrsimplexamples/blob/master/openvrsimplexamples/src/utils.cpp
+//-----------------------------------------------------------------------------
+std::string GetTrackedDeviceString(vr::IVRSystem *pHmd, vr::TrackedDeviceIndex_t unDevice, vr::TrackedDeviceProperty prop, vr::TrackedPropertyError *peError = NULL)
+{
+        uint32_t requiredBufferLen = pHmd->GetStringTrackedDeviceProperty(unDevice, prop, NULL, 0, peError);
+        if( requiredBufferLen == 0 )
+                return "";
+
+        char *pchBuffer = new char[requiredBufferLen];
+        requiredBufferLen = pHmd->GetStringTrackedDeviceProperty(unDevice,prop,pchBuffer,requiredBufferLen,peError);
+        std::string sResult = pchBuffer;
+        delete[] pchBuffer;
+
+        return sResult;
+}
+
+//-----------------------------------------------------------------------------
+// Purpose: helper to get a string from a tracked device type class. FROM https://github.com/matinas/openvrsimplexamples/blob/master/openvrsimplexamples/src/utils.cpp
+//-----------------------------------------------------------------------------
+std::string GetTrackedDeviceClassString(vr::ETrackedDeviceClass td_class) {
+
+        std::string str_td_class = "Unknown class";
+
+        switch (td_class)
+        {
+        case vr::TrackedDeviceClass_Invalid:			// = 0, the ID was not valid.
+                str_td_class = "invalid";
+                break;
+        case vr::TrackedDeviceClass_HMD:				// = 1, Head-Mounted Displays
+                str_td_class = "hmd";
+                break;
+        case vr::TrackedDeviceClass_Controller:			// = 2, Tracked controllers
+                str_td_class = "controller";
+                break;
+        case vr::TrackedDeviceClass_GenericTracker:		// = 3, Generic trackers, similar to controllers
+                str_td_class = "generic tracker";
+                break;
+        case vr::TrackedDeviceClass_TrackingReference:	// = 4, Camera and base stations that serve as tracking reference points
+                str_td_class = "base station";
+                break;
+        case vr::TrackedDeviceClass_DisplayRedirect:	// = 5, Accessories that aren't necessarily tracked themselves, but may redirect video output from other tracked devices
+                str_td_class = "display redirect";
+                break;
+        }
+
+        return str_td_class;
+}
+
 
 int main(int argc, char **argv)
 {
@@ -59,14 +114,17 @@ int main(int argc, char **argv)
     ros::NodeHandle nh;
     image_transport::ImageTransport it(nh);
 
+    //initialize OpenVR
+    if (init_OpenVR() != 0) return -1;
+
     //define and generate textures. We're clamping the textures and using linear interpolation for now. OpenGL information on textures is from https://open.gl/textures
     GLuint tex_left;
     glGenTextures(1,&tex_left);
 
     GLuint tex_right;
     glGenTextures(1,&tex_right);
-    int pnWidth;
-    int pnHeight;
+    uint32_t pnWidth;
+    uint32_t pnHeight;
     vr::TrackedDevicePose_t m_rTrackedDevicePose[ vr::k_unMaxTrackedDeviceCount ];
 
     vr_system->GetRecommendedRenderTargetSize(&pnWidth, &pnHeight );
@@ -131,60 +189,59 @@ int init_OpenVR()
         // Check whether there is an HMD plugged-in and the SteamVR runtime is installed
         if (vr::VR_IsHmdPresent())
         {
-                cout << "An HMD was successfully found in the system" << endl;
+                std::cout << "An HMD was successfully found in the system" << std::endl;
 
                 if (vr::VR_IsRuntimeInstalled()) {
-                        const char* runtime_path = vr::VR_RuntimePath();
-                        cout << "Runtime correctly installed at '" << runtime_path << "'" << endl;
+                        std::cout << "Runtime correctly installed" << std::endl;
                 }
                 else
                 {
-                        cout << "Runtime was not found, quitting app" << endl;
+                        std::cout << "Runtime was not found, quitting app" << std::endl;
                         return -1;
                 }
         }
         else
         {
-                cout << "No HMD was found in the system, quitting app" << endl;
+                std::cout << "No HMD was found in the system, quitting app" << std::endl;
                 return -1;
         }
 
         // Load the SteamVR Runtime
         vr::HmdError err;
-        vr_context = vr::VR_Init(&err,vr::EVRApplicationType::VRApplication_Scene);
-        vr_context == NULL ? cout << "Error while initializing SteamVR runtime. Error code is " << vr::VR_GetVRInitErrorAsSymbol(err) << endl : cout << "SteamVR runtime successfully initialized" << endl;
+        vr_system = vr::VR_Init(&err,vr::EVRApplicationType::VRApplication_Scene);
+        vr_system == NULL ? std::cout << "Error while initializing SteamVR runtime. Error code is " << vr::VR_GetVRInitErrorAsSymbol(err) << std::endl : std::cout << "SteamVR runtime successfully initialized" << std::endl;
 
         // Obtain some basic information given by the runtime
         int base_stations_count = 0;
         for (uint32_t td=vr::k_unTrackedDeviceIndex_Hmd; td<vr::k_unMaxTrackedDeviceCount; td++) {
 
-                if (vr_context->IsTrackedDeviceConnected(td))
+                if (vr_system->IsTrackedDeviceConnected(td))
                 {
-                        vr::ETrackedDeviceClass tracked_device_class = vr_context->GetTrackedDeviceClass(td);
+                        vr::ETrackedDeviceClass tracked_device_class = vr_system->GetTrackedDeviceClass(td);
 
-                        string td_type = GetTrackedDeviceClassString(tracked_device_class);
+                        std::string td_type = GetTrackedDeviceClassString(tracked_device_class);
                         tracked_device_type[td] = td_type;
 
-                        cout << "Tracking device " << td << " is connected " << endl;
-                        cout << "  Device type: " << td_type << ". Name: " << GetTrackedDeviceString(vr_context,td,vr::Prop_TrackingSystemName_String) << endl;
+                        std::cout << "Tracking device " << td << " is connected " << std::endl;
+                        std::cout << "  Device type: " << td_type << ". Name: " << GetTrackedDeviceString(vr_system,td,vr::Prop_TrackingSystemName_String) << std::endl;
 
                         if (tracked_device_class == vr::ETrackedDeviceClass::TrackedDeviceClass_TrackingReference) base_stations_count++;
 
                         if (td == vr::k_unTrackedDeviceIndex_Hmd)
                         {
                                 // Fill variables used for obtaining the device name and serial ID (used later for naming the SDL window)
-                                driver_name = GetTrackedDeviceString(vr_context,vr::k_unTrackedDeviceIndex_Hmd,vr::Prop_TrackingSystemName_String);
-                                driver_serial = GetTrackedDeviceString(vr_context,vr::k_unTrackedDeviceIndex_Hmd,vr::Prop_SerialNumber_String);
+                                driver_name = GetTrackedDeviceString(vr_system,vr::k_unTrackedDeviceIndex_Hmd,vr::Prop_TrackingSystemName_String);
+                                driver_serial = GetTrackedDeviceString(vr_system,vr::k_unTrackedDeviceIndex_Hmd,vr::Prop_SerialNumber_String);
                         }
                 }
                 else
-                        cout << "Tracking device " << td << " not connected" << endl;
+                        std::cout << "Tracking device " << td << " not connected" << std::endl;
         }
 
         // Check whether both base stations are found, not mandatory but just in case...
         if (base_stations_count < 2)
         {
-                cout << "There was a problem indentifying the base stations, please check they are powered on" << endl;
+                std::cout << "There was a problem indentifying the base stations, please check they are powered on" << std::endl;
 
                 return -1;
         }
